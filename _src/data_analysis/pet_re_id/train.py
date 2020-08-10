488aec91525bf76b1e5cd5eca04cc2f3d1329799
TaeYang Kwon <sunnight9507@naver.com>
2020-08-11 01:17:53 +0900

    file 이동

--- _src/data_analysis/pet_re_id/train.py
+++ _src/data_analysis/pet_re_id/train.py
@@ -371,8 +371,8 @@ dir_name = os.path.join('./model',name)
 if not os.path.isdir(dir_name):
     os.mkdir(dir_name)
 #record every run
-copyfile('./train.py', dir_name+'/train.py')
-copyfile('./model.py', dir_name+'/model.py')
+copyfile('train.py', dir_name + '/train.py')
+copyfile('model.py', dir_name + '/model.py')
 
 # save opts
 with open('%s/opts.yaml'%dir_name,'w') as fp:
