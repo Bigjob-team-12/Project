233e81991cf9474ceb9b449e8bc02c5285160b9a
mocha-123 <52557577+mocha-123@users.noreply.github.com>
2020-08-18 18:58:58 +0900

    주석 (#33)
    * comment

--- _src/data_collection/data_collection_imagenet.py
+++ _src/data_collection/data_collection_imagenet.py
@@ -6,14 +6,20 @@ from tqdm import tqdm
 import socket
 socket.setdefaulttimeout(180)
 
+# text 폴더에 저장된 text 파일에서 url list 추출
 file_list = os.listdir('text')
+
 for file in file_list:
+    # text file명으로 폴더 생성
     directory = file.split('.')[0]
     os.makedirs(os.path.join(directory))
+
     i = 0
     with open('text/'+file, 'r', encoding='ISO-8859-1') as f:
         strings = f.readlines()
         print(directory, len(strings))
+
+        # 각 url에서 이미지 저장
         for item in tqdm(strings):
             if len(item) > 3:
                 try:
