1e414583cb1f85ce84766053a3b6fb0ffbb7ff56
mocha-123 <vanillab9@daum.net>
2020-08-18 18:57:30 +0900

    comment

--- _src/data_collection/data_collection_google_image.py
+++ _src/data_collection/data_collection_google_image.py
@@ -5,18 +5,18 @@ from urllib.request import urlretrieve
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-socket.setdefaulttimeout(30)
+socket.setdefaulttimeout(30) # 장기 미응답 처리
 driver = webdriver.Chrome('chromedriver.exe')
 
-query = '진돗개'
+# selenium - google image search
+query = '진돗개' # 검색어
 url = "https://www.google.com/search?q="+query+"&sourceid=chrome&ie=UTF-8&source=lnms&tbm=isch&tbs=itp:photo"
 driver.get(url)
 driver.maximize_window()
 
-#scroll
+# scroll down
 last_height = driver.execute_script("return document.body.scrollHeight")
 clicked = False
-
 while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(1)
@@ -26,7 +26,7 @@ while True:
         if clicked is True:
             break
         else:
-            if driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input').is_displayed():
+            if driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input').is_displayed(): # 결과 더보기 버튼 클릭
                 driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input').click()
                 clicked = True
             elif NoSuchElementException:
@@ -34,20 +34,22 @@ while True:
     
     last_height = new_height
 
-div = driver.find_element_by_xpath('//*[@id="islrg"]/div[1]')
-img_list = div.find_elements_by_css_selector(".rg_i.Q4LuWd")
+# 개별 이미지 탐색
+div = driver.find_element_by_xpath('//*[@id="islrg"]/div[1]') # 전체 이미지 영역
+img_list = div.find_elements_by_css_selector(".rg_i.Q4LuWd") # 개별 이미지 영역
 
-#save
+# 저장 폴더 생성
 if os.path.isdir(os.path.join('more_data', 'google', query)) is False:
     os.makedirs(os.path.join('more_data', 'google', query))
 
+# 각 이미지 클릭 후 원본 이미지 url에서 이미지 download
 i = 0
 for img in img_list:
     try:   
         img.click()
         driver.implicitly_wait(3)
-        src = driver.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute('src')
-        urlretrieve(src, 'more_data/google/{}/{}.jpg'.format(query, i))
+        src = driver.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute('src') # 원본 url
+        urlretrieve(src, 'more_data/google/{}/{}.jpg'.format(query, i)) #저장
         i += 1
     except Exception as e:
         pass
