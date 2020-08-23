2c6bcf7913c4004a76a758c4252a322d27e720b8
mocha-123 <52557577+mocha-123@users.noreply.github.com>
2020-08-24 02:26:41 +0900

    git conflict 해결

--- _src/data_collection/data_collection_google_image.py
+++ _src/data_collection/data_collection_google_image.py
@@ -5,46 +5,18 @@ from urllib.request import urlretrieve
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 socket.setdefaulttimeout(30) # 장기 미응답 처리
 driver = webdriver.Chrome('chromedriver.exe')
 
 # selenium - google image search
 query = '진돗개' # 검색어
-=======
-socket.setdefaulttimeout(30)
-driver = webdriver.Chrome('chromedriver.exe')
-
-query = '진돗개'
->>>>>>> 6f1b3ba... dataset collection
-=======
-socket.setdefaulttimeout(30) # 장기 미응답 처리
-driver = webdriver.Chrome('chromedriver.exe')
-
-# selenium - google image search
-query = '진돗개' # 검색어
->>>>>>> 1e41458... comment
 url = "https://www.google.com/search?q="+query+"&sourceid=chrome&ie=UTF-8&source=lnms&tbm=isch&tbs=itp:photo"
 driver.get(url)
 driver.maximize_window()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 # scroll down
 last_height = driver.execute_script("return document.body.scrollHeight")
 clicked = False
-=======
-#scroll
-last_height = driver.execute_script("return document.body.scrollHeight")
-clicked = False
-
->>>>>>> 6f1b3ba... dataset collection
-=======
-# scroll down
-last_height = driver.execute_script("return document.body.scrollHeight")
-clicked = False
->>>>>>> 1e41458... comment
 while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(1)
@@ -54,15 +26,7 @@ while True:
         if clicked is True:
             break
         else:
-<<<<<<< HEAD
-<<<<<<< HEAD
             if driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input').is_displayed(): # 결과 더보기 버튼 클릭
-=======
-            if driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input').is_displayed():
->>>>>>> 6f1b3ba... dataset collection
-=======
-            if driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input').is_displayed(): # 결과 더보기 버튼 클릭
->>>>>>> 1e41458... comment
                 driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input').click()
                 clicked = True
             elif NoSuchElementException:
@@ -70,8 +34,6 @@ while True:
     
     last_height = new_height
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 # 개별 이미지 탐색
 div = driver.find_element_by_xpath('//*[@id="islrg"]/div[1]') # 전체 이미지 영역
 img_list = div.find_elements_by_css_selector(".rg_i.Q4LuWd") # 개별 이미지 영역
@@ -81,41 +43,13 @@ if os.path.isdir(os.path.join('more_data', 'google', query)) is False:
     os.makedirs(os.path.join('more_data', 'google', query))
 
 # 각 이미지 클릭 후 원본 이미지 url에서 이미지 download
-=======
-div = driver.find_element_by_xpath('//*[@id="islrg"]/div[1]')
-img_list = div.find_elements_by_css_selector(".rg_i.Q4LuWd")
-=======
-# 개별 이미지 탐색
-div = driver.find_element_by_xpath('//*[@id="islrg"]/div[1]') # 전체 이미지 영역
-img_list = div.find_elements_by_css_selector(".rg_i.Q4LuWd") # 개별 이미지 영역
->>>>>>> 1e41458... comment
-
-# 저장 폴더 생성
-if os.path.isdir(os.path.join('more_data', 'google', query)) is False:
-    os.makedirs(os.path.join('more_data', 'google', query))
-
-<<<<<<< HEAD
->>>>>>> 6f1b3ba... dataset collection
-=======
-# 각 이미지 클릭 후 원본 이미지 url에서 이미지 download
->>>>>>> 1e41458... comment
 i = 0
 for img in img_list:
     try:   
         img.click()
         driver.implicitly_wait(3)
-<<<<<<< HEAD
-<<<<<<< HEAD
         src = driver.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute('src') # 원본 url
         urlretrieve(src, 'more_data/google/{}/{}.jpg'.format(query, i)) #저장
-=======
-        src = driver.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute('src')
-        urlretrieve(src, 'more_data/google/{}/{}.jpg'.format(query, i))
->>>>>>> 6f1b3ba... dataset collection
-=======
-        src = driver.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute('src') # 원본 url
-        urlretrieve(src, 'more_data/google/{}/{}.jpg'.format(query, i)) #저장
->>>>>>> 1e41458... comment
         i += 1
     except Exception as e:
         pass
--- _src/data_collection/data_collection_imagenet.py
+++ _src/data_collection/data_collection_imagenet.py
@@ -6,10 +6,6 @@ from tqdm import tqdm
 import socket
 socket.setdefaulttimeout(180)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1e41458... comment
 # text 폴더에 저장된 text 파일에서 url list 추출
 file_list = os.listdir('text')
 
@@ -18,29 +14,12 @@ for file in file_list:
     directory = file.split('.')[0]
     os.makedirs(os.path.join(directory))
 
-<<<<<<< HEAD
-=======
-file_list = os.listdir('text')
-for file in file_list:
-    directory = file.split('.')[0]
-    os.makedirs(os.path.join(directory))
->>>>>>> 6f1b3ba... dataset collection
-=======
->>>>>>> 1e41458... comment
     i = 0
     with open('text/'+file, 'r', encoding='ISO-8859-1') as f:
         strings = f.readlines()
         print(directory, len(strings))
-<<<<<<< HEAD
-<<<<<<< HEAD
 
         # 각 url에서 이미지 저장
-=======
->>>>>>> 6f1b3ba... dataset collection
-=======
-
-        # 각 url에서 이미지 저장
->>>>>>> 1e41458... comment
         for item in tqdm(strings):
             if len(item) > 3:
                 try:
